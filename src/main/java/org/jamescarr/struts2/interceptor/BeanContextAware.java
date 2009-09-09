a587f46358120d395b362960d5c0247805b62549
jamescarr <jamescarr@abidos.(none)>
2009-09-08 21:45:07 -0500

    using WebContextUtils

--- src/main/java/org/jamescarr/struts2/interceptor/BeanContextAware.java
+++ src/main/java/org/jamescarr/struts2/interceptor/BeanContextAware.java
@@ -2,35 +2,32 @@ package org.jamescarr.struts2.interceptor;
 
 import java.util.Map;
 
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.ApplicationAware;
-import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
-public class BeanContextAware extends AbstractInterceptor implements ApplicationContextAware, ApplicationAware{
+public class BeanContextAware extends AbstractInterceptor implements
+		ApplicationAware {
 
-	private ApplicationContext context;
 	private Map contextMap;
 
 	public String intercept(ActionInvocation invocation) throws Exception {
-		for(String beanName : context.getBeanDefinitionNames()){
+		ApplicationContext context = WebApplicationContextUtils
+				.getWebApplicationContext(ServletActionContext
+						.getServletContext());
+		for (String beanName : context.getBeanDefinitionNames()) {
 			contextMap.put(beanName, context.getBean(beanName));
 		}
 		return invocation.invoke();
 	}
 
-	public void setApplicationContext(ApplicationContext context)
-			throws BeansException {
-				this.context = context;
-		
-	}
-
 	public void setApplication(Map contextMap) {
 		this.contextMap = contextMap;
-		
+
 	}
 
 }
